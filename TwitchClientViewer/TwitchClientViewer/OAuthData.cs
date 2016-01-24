09d5303399dcda1bf59d2e35ce2b04ed7667b4a4
Rui Guerra <rui.guerra@frotcom.com>
2016-01-24 22:14:33 +0000

    changed data

--- TwitchClientViewer/TwitchClientViewer/OAuthData.cs
+++ TwitchClientViewer/TwitchClientViewer/OAuthData.cs
@@ -8,7 +8,7 @@ namespace TwitchClientViewer
 {
     class OAuthData
     {
-        public static string clientId = "hsmk7ppub7n6e9puercdyvlj8o9yppg";
+        public static string clientId = "use your twitch clientid";
         public static string clientSecret = string.Empty;
         public static string token = string.Empty;
     }
